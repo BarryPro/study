7e3df77ff0e8eb46baf1eb88da443dec35fc9659
于成龙 <yuchenglong03@meituan.com>
2019-04-17 23:55:06 +0800

    Initial commit

---
+++ study_demo/src/main/java/com/barry/study/reflect/CreateFun.java
@@ -0,0 +1,26 @@
+package com.barry.study.reflect;
+
+
+import com.barry.study.basic.DeepClone;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Created by belong on 2016/12/11.
+ */
+public class CreateFun
+{
+    public static void main(String[] args)
+            throws Exception
+    {
+        //获取JFrame对应的Class对象
+        Class<?> jframeClazz = Class.forName("com.belong.basic.DeepClone");//引入该目录下的jar包中的A类
+        //获取JFrame中带一个字符串参数的构造器
+        String name="belong";
+        Constructor ctor = jframeClazz.getConstructor(String.class);
+        //调用Constructor的newInstance方法创建对象
+        Object obj = ctor.newInstance(name);
+        //输出JFrame对象
+        ((DeepClone)obj).fun();
+    }
+}
