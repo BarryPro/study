65307b0307898f6d3c91cdcec5748164809c605b
BarryPro <belong.belong@outlook.com>
2022-08-21 14:38:07 +0800

    增加链表求和

---
+++ study_demo/src/main/java/com/barry/study/list/ListNode.java
@@ -0,0 +1,22 @@
+package com.barry.study.list;
+
+/**
+ * Definition for singly-linked list.
+ * 单链表节点定义
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ study_demo/src/main/java/com/barry/study/practice/AddTwoNumbersDemo.java
@@ -0,0 +1,42 @@
+package com.barry.study.practice;
+
+import com.barry.study.list.ListNode;
+
+public class AddTwoNumbersDemo {
+    public static void main(String[] args) {
+        System.out.println(addTwoNumbers(null, null));
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        // 异常case检查
+        if (l1 == null || l2 == null) {
+            return root.next;
+        }
+        // 从头开始遍历
+        ListNode cursor = root;
+        // 进位标识
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int l1var = l1 != null ? l1.val : 0;
+            int l2var = l2 != null ? l2.val : 0;
+            int sum = l1var + l2var + carry;
+            carry = sum / 10;
+
+            ListNode sumNode = new ListNode(sum % 10);
+            // 链接节点操作
+            cursor.next = sumNode;
+            // 游标赋值
+            cursor = sumNode;
+            // 移动游标
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        return root.next;
+    }
+}
